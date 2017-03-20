2dee82456214228e45422f9f4351c5ce7dbae066
Henry Lau <henrywlau@yahoo.com>
2017-03-20 02:38:11 -0500

    Completed display of jobs

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -27,11 +27,13 @@ public class SearchController {
     public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         if (searchType.equals("all")) {
             ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("columns", ListController.columnChoices);
             model.addAttribute("title", "All Jobs with " + searchTerm);
             model.addAttribute("jobs", jobs);
             return "search";
         } else {
             ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("columns", ListController.columnChoices);
             model.addAttribute("title", "Jobs with " + searchType + ": " + searchTerm);
             model.addAttribute("jobs", jobs);
             return "search";
